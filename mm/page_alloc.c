d874102c8aa93ddf34629f0b558ff597d8c88386
KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
2009-07-13 15:01:12 +0900

    add buffer cache information to show_free_areas()
    It is often useful to know the statistics for all pages that are handled
    like page cache pages when looking at OOM log output.
    
    Therefore show_free_areas() should also display buffer cache statistics.
    
    Signed-off-by: KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
    Acked-by: Wu Fengguang <fengguang.wu@intel.com>
    Reviewed-by: Rik van Riel <riel@redhat.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1899,7 +1899,7 @@ void show_free_areas(void)
 #ifdef CONFIG_UNEVICTABLE_LRU
 		" unevictable:%lu"
 #endif
-		" dirty:%lu writeback:%lu unstable:%lu\n"
+		" dirty:%lu writeback:%lu unstable:%lu buffer:%lu\n"
 		" free:%lu slab:%lu mapped:%lu pagetables:%lu bounce:%lu\n",
 		global_page_state(NR_ACTIVE_ANON),
 		global_page_state(NR_ACTIVE_FILE),
@@ -1911,6 +1911,7 @@ void show_free_areas(void)
 		global_page_state(NR_FILE_DIRTY),
 		global_page_state(NR_WRITEBACK),
 		global_page_state(NR_UNSTABLE_NFS),
+		nr_blockdev_pages(),
 		global_page_state(NR_FREE_PAGES),
 		global_page_state(NR_SLAB_RECLAIMABLE) +
 			global_page_state(NR_SLAB_UNRECLAIMABLE),
